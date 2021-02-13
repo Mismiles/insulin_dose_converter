e407161326233d55a62e19e5891f01d25b7d88ce
Msmiles <muhammadirfan123@hotmail.com>
2021-02-13 17:46:27 +0000

    added function to show insulin doses for apidra

--- js/global.js
+++ js/global.js
@@ -1,1 +1,11 @@
  
+ function sInsulin() {
+    var rapid = document.getElementById('rapid');
+    var apidra = document.getElementById("apidra");
+    
+    if (apidra.selected) {
+        rapid.style.display = 'block';
+    } else {
+        rapid.style.display = 'none';
+    }
+}
