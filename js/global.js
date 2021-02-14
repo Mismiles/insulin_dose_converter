5264f6a9e419bcb04ce9aa54b62aaad1cebfd9fb
Msmiles <muhammadirfan123@hotmail.com>
2021-02-14 12:19:50 +0000

    added humulin m3 dose entry

--- js/global.js
+++ js/global.js
@@ -2,11 +2,10 @@
  function sInsulin() {
     var rapid = document.getElementById('rapid');
     var midLong = document.getElementById('midLong');
-    var BD = document.getElementById('BD');
+    var humm3 = document.getElementById('humm3');
     var odORbd = document.getElementById('odORbd');
     var bdORtd = document.getElementById('bdORtd');
     var odTOtd = document.getElementById('odTOtd');
-
 
     var apidra = document.getElementById("apidra");
     var humalog = document.getElementById("humalog");
@@ -14,7 +13,7 @@
     var humulinI = document.getElementById('humulinI');
     var humalogm25 = document.getElementById("humalogm25");
     var humalogm50 = document.getElementById("humalogm50");
-    // var humulinm3 = document.getElementById("humulinm3");
+    var humulinm3 = document.getElementById("humulinm3");
     var levemir = document.getElementById("levemir");
     var novom30 = document.getElementById("novom30");
     var tresiba = document.getElementById("tresiba");
@@ -24,31 +23,38 @@
         rapid.style.display = 'block';
         midLong.style.display = 'none';
         odORbd.style.display = 'none'; 
-        BD.style.display = 'none';
+        humm3.style.display = 'none';
         odTOtd.style.display = 'none';
         
     } else if (tresiba.selected || toujeo.selected) {
         midLong.style.display = 'block';
         rapid.style.display = 'none';
-        BD.style.display = 'none';
+        humm3.style.display = 'none';
         odORbd.style.display = 'none'; 
         odTOtd.style.display = 'none';
     } else if (humalogm25.selected || levemir.selected || humulinI.selected) {
         odORbd.style.display = 'block';
         midLong.style.display = 'none';
         rapid.style.display = 'none';
-        BD.style.display = 'none';
+        humm3.style.display = 'none';
         odTOtd.style.display = 'none';
     } else if (humalogm50.selected || novom30.selected) {
         odORbd.style.display = 'none';
         midLong.style.display = 'none';
         rapid.style.display = 'none';
-        BD.style.display = 'none';
+        humm3.style.display = 'none';
         odTOtd.style.display = 'block';
+    } else if (humulinm3.selected) {
+        odORbd.style.display = 'none';
+        midLong.style.display = 'none';
+        rapid.style.display = 'none';
+        humm3.style.display = 'block';
+        odTOtd.style.display = 'none';
+
     } else {
         rapid.style.display = 'none';
         midLong.style.display = 'none';
-        BD.style.display = 'none';
+        humm3.style.display = 'none';
         odORbd.style.display = 'none';
         odTOtd.style.display = 'none';       
     }
